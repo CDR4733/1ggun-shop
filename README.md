08b2792ac73cc920716c6fa48a91e1a15e48ea58
CDR4733 <shsing2@naver.com>
2024-05-27 18:50:34 +0900

    feat : 프로젝트 기본 세팅

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 1ggun-shop
+
+## 🍓 API Specification
+
+⛱️Link: https://shocking-flavor-85b.notion.site/API-e2ad44c6fa1e41e7a776e310f44b8d37
+
+(수정중)
+
+## 🥕 Entity Relationship Diagram
+
+⛱️Link: https://shocking-flavor-85b.notion.site/ERD-a7cf7877702e4488a139508d3c6e1f5f?pvs=4
+
+## 🍉 Folder Structures
+
+(수정중)
+
+🍊🍋🍎🥭🍍🍑🍏🍒🍌
---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+// import LogMiddleware from './src/middlewares/log.middleware.js'; // LogMiddlewaqre 완성되면 주석 풀기!!
+import UsersRouter from "./src/routes/users.router.js";
+import errorHandlerMiddleware from "./src/middlewares/error-handler.middleware.js";
+
+const app = express();
+const PORT = 3018;
+
+// app.use(LogMiddleware); // LogMiddlewaqre 완성되면 주석 풀기!! 맨 위에 위치할 것!
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", [UsersRouter]);
+app.use(errorHandlerMiddleware); // errorHandlerMiddleware는 마지막에 위치할 것!
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
