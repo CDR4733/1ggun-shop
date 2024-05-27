12481e9b69eaf65267b695bded6ed4c12bae861f
CDR4733 <shsing2@naver.com>
2024-05-27 19:57:59 +0900

    feat : DB연결, 스키마 작성, 테이블 생성

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 // import LogMiddleware from './src/middlewares/log.middleware.js'; // LogMiddlewaqre 완성되면 주석 풀기!!
+import { prisma } from "./src/utils/prisma.util.js";
+
 import UsersRouter from "./src/routes/users.router.js";
 import errorHandlerMiddleware from "./src/middlewares/error-handler.middleware.js";
 
@@ -15,6 +17,7 @@ app.use(cookieParser());
 app.use("/api", [UsersRouter]);
 app.use(errorHandlerMiddleware); // errorHandlerMiddleware는 마지막에 위치할 것!
 
+prisma.$connect();
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
 });
